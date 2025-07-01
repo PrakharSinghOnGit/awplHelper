d2a3229e228620fb8cc450c95806d038073b23ef
PrakharSinghOnGit <shaansinghd1209@gmail.com>
2025-07-01 18:31:57 +0530

    working edit page

--- app/(dashboard)/edit-team/page.tsx
+++ app/(dashboard)/edit-team/page.tsx
@@ -1,21 +1,179 @@
 "use client";
 
+import * as React from "react";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Table, TableHeader } from "@/components/ui/table";
-import { DownloadIcon, Plus, Search } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+import { TeamMember } from "@/types";
+import { DownloadIcon, Plus, Search, ArrowUpDown, Edit } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
+
+const data: TeamMember[] = [
+  {
+    id: "aabbcc",
+    level: "Ambassador",
+    pass: "5135",
+    name: "vv",
+  },
+  {
+    id: "ababab",
+    level: "Ambassador",
+    pass: "5135",
+    name: "b",
+  },
+  {
+    id: "abcabc",
+    level: "Ambassador",
+    pass: "5135",
+    name: "c",
+  },
+  {
+    id: "acacac",
+    level: "Ambassador",
+    pass: "5135",
+    name: "de",
+  },
+  {
+    id: "cacaca",
+    level: "Ambassador",
+    pass: "5135",
+    name: "fg",
+  },
+  {
+    id: "bcbcbc",
+    level: "Ambassador",
+    pass: "5135",
+    name: "zz",
+  },
+];
+
+function handleEdit(id: string) {
+  console.log(id);
+}
+
+function handleExport() {}
+
+export const columns: ColumnDef<TeamMember>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="has-[>svg]:px-0 m-0 p-0 w-full rounded-none justify-start"
+          variant="ghost"
+          onClick={() =>
+            column.toggleSorting(
+              !column.getIsSorted()
+                ? false
+                : column.getIsSorted() === "asc"
+                ? true
+                : false
+            )
+          }
+        >
+          Name
+          <ArrowUpDown strokeWidth={2} />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "id",
+    header: "User Id",
+    cell: ({ row }) => <div className="uppercase">{row.getValue("id")}</div>,
+  },
+  {
+    accessorKey: "pass",
+    header: "Pass",
+    cell: ({ row }) => row.getValue("pass"),
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    header: "Edit",
+    cell: ({ row }) => {
+      return (
+        <Button onClick={() => handleEdit(row.id)} variant="outline" size="sm">
+          <Edit />
+        </Button>
+      );
+    },
+  },
+];
 
 export default function EditTeam() {
-  function handleAdd() {}
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [rowSelection, setRowSelection] = React.useState({});
 
-  function handleExport() {}
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      rowSelection,
+    },
+  });
 
   return (
     <div className="flex flex-col gap-4 w-full pt-3 grow">
       <div className="flex flex-row gap-2">
-        <Input icon={<Search size={15} />} placeholder="Search User" />
+        <Input
+          icon={<Search size={15} />}
+          placeholder="Search..."
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+        />
         <Button
-          onClick={handleAdd}
+          onClick={() => handleEdit("")}
           variant="default"
           className="font-semibold gap-1"
         >
@@ -29,7 +187,52 @@ export default function EditTeam() {
       </div>
       <div className="border-2 border-color-border grow rounded-md overflow-scroll">
         <Table>
-          <TableHeader></TableHeader>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
         </Table>
       </div>
     </div>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  fastRefresh: true,
+  concurrentFeatures: true,
+  productionBrowserSourceMaps: false, // Disable source maps in development
+  optimizeFonts: false, // Disable font optimization
+  minify: false, // Disable minification
+
   /* config options here */
 };
 
